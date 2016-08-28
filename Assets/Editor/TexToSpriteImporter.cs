d1bafe12d00e8858e4cf1b950c026f4748487a88
Sugawara <comic.bonbon64@gmail.com>
2016-08-28 17:14:25 +0900

    TextureImport時のデフォルト設定を行う処理を追加

---
+++ Assets/Editor/TexToSpriteImporter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class TexToSpriteImporter : AssetPostprocessor
+{
+	void OnPreprocessTexture()
+	{
+		// Import字のデフォルト設定
+		var importer = assetImporter as TextureImporter;
+
+		importer.textureType = TextureImporterType.Sprite;
+	}
+}
